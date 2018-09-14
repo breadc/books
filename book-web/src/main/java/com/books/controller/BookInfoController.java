a35eb037f1b79a4c40e9caac7c61a3e0b59caf3a
chenghui <18500208274@163.com>
2018-09-14 16:46:15 +0800

    提交路径

---
+++ book-web/src/main/java/com/books/controller/BookInfoController.java
@@ -0,0 +1,17 @@
+package com.books.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 版本号 v1
+ * @author chenghui
+ *
+ */
+@RequestMapping("/v1/book")
+@Controller
+public class BookInfoController {
+		
+	
+	
+	
+}
---
+++ book-web/src/main/java/com/books/service/BookInfoService.java
@@ -0,0 +1,5 @@
+package com.books.service;
+
+public interface BookInfoService {
+
+}
